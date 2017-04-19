f222e953068146e820363d3e2041ad64ec70f430
Nikhil <nikhiljerry009@gmail.com>
2017-04-19 16:05:21 +0530

    Updated OSE commons.js

--- OpenScriptingEnd/commons.js
+++ OpenScriptingEnd/commons.js
@@ -1,11 +1,32 @@
-function type(pack){
-    return Java.type(pack);
+var RawTCPServer = type("ose.raw.RawTCPServer");
+var RawTCPClient = type("ose.raw.RawTCPClient");
+var File = type("java.io.File");
+var Scanner = type("java.util.Scanner");
+var Logger = type("java.util.logging.Logger");
+var LoggingLevel = type("java.util.logging.Level");
+
+function type(program){
+    return Java.type(program);
 }
 function extend(type, abstracts){
     return Java.extend(type, abstracts);
 }
-function getSuper(Extend){
-    return Java.super(Extend);
+
+function getSuper(extender_instance){
+    return Java.super(extender_instance);
 }
 
-var RawTCPServer = type("ose.raw.RawTCPServer");
+function alert(msg){
+    var JOptionPane = type("javax.swing.JOptionPane");
+    JOptionPane.showMessageDialog(null, msg);
+}
+
+function input(prompt){
+    var key = new Scanner(java.lang.System.in);
+    java.lang.System.out.print(prompt);
+    return key.nextLine();
+}
+
+function getlogger(name){
+    return Logger.getLogger(name);
+}
